b32af11cf4a507aff9a04cac83b2a7b12a34cad0
Phuong Truong <ptruong5260@gmail.com>
2025-02-21 22:03:10 -0500

    Add more contents for readme about the application, database consideration

--- main.py
+++ main.py
@@ -1,41 +1,47 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-import single_match_service
+import oddsmath_crawler_service
+import argparse
+import re
+from urllib.parse import urlparse
 
 def main():
+    parser = argparse.ArgumentParser(description="Pass a date link from oddsmath to get matches' bet data.", allow_abbrev=False)
+    # Define CLI arguments
+    parser.add_argument("--oddsmath_link", type=str, required=True, help="Pass a date link")
+    # Parse CLI arguments
+    args = parser.parse_args()
+    if args.oddsmath_link:
+        oaddsmath_link = args.oddsmath_link
 
-    # set up ChromeOptions
-    options = webdriver.ChromeOptions()
+        if is_oddsmath_link(oaddsmath_link) is False:
+            print(f'The application supports only oddsmath.com links.')
+            return
+        
+        # Most pages have the events info
+        event_links = oddsmath_crawler_service.get_date_event_links(oaddsmath_link)
+        print(f'EVENT_LINKS\n{event_links}')
 
-    # add headless Chrome option
-    options.add_argument("--headless=new")
+        if is_date_link(oaddsmath_link):
+            match_links = oddsmath_crawler_service.get_match_links_for_a_date(oaddsmath_link)
+            print(f'\nMATCH_LINKS\n{match_links}')
 
-    # set up Chrome in headless mode
-    driver = webdriver.Chrome(options=options)
+        if is_match_link(oaddsmath_link):
+            bet_data = oddsmath_crawler_service.get_bet_data_for_a_match(oaddsmath_link)
+            print(f'\nBET_DATA\n{bet_data}')
 
-    # open the target website
-    match_link = "https://www.oddsmath.com/football/international/afc-asian-cup-clubs-2424/2025-02-20/jeonbuk-hyundai-motors-vs-port-fc-4714094/"
-    # driver.get("https://www.scrapingcourse.com/ecommerce/")
-    # driver.get("https://www.oddsmath.com/football/international/afc-asian-cup-clubs-2424/2025-02-20/jeonbuk-hyundai-motors-vs-port-fc-4714094/")
+def is_oddsmath_link(oddsmath_link: str):
+    parsed_url = urlparse(oddsmath_link)
+    return parsed_url.netloc.endswith("oddsmath.com")
 
-    # print the current URL and page title
-    # print(f"Page URL: {driver.current_url}")
-    # print(f"Page Title: {driver.title}")
+def is_date_link(oddsmath_link: str):
+    pattern = r"^https://www\.oddsmath\.com/football/matches/(today|\d{4}-\d{2}-\d{2})/$"
+    return bool(re.match(pattern, oddsmath_link))
 
-    match_bet_data = single_match_service.get_bet_data_for_a_match(driver, match_link)
+def is_match_link(oaddsmath_link: str):
+    pattern = re.compile(
+        r"^https://www\.oddsmath\.com/football/[\w-]+/[\w-]+-\d+/"
+        r"\d{4}-\d{2}-\d{2}/[\w-]+-vs-[\w-]+-\d+/$"
+    )
+    return bool(pattern.match(oaddsmath_link))
 
-    #     # append the scraped data to the empty list
-    #     scraped_data.append(data)
-
-    # print the scraped data
-    print(match_bet_data)
-
-    # close the driver instance and release its resources
-    driver.quit()
-
-# get bet data for a match link
-# data returns with a list of elements {name, odds_1, odds_X, odds_2}
-
-
-
-main()
+if __name__ == "__main__":
+    main()
---
+++ oddsmath_selenium/oddsmath_selenium_service.py
@@ -0,0 +1,64 @@
+from typing import List, Callable, TypeVar
+from selenium import webdriver
+from oddsmath_selenium.match_page import MatchPage
+from oddsmath_selenium.date_page import DatePage
+from oddsmath_selenium.home_page import HomePage
+
+T = TypeVar("T")  # Generic return type for functions that use WebDriver
+
+def with_driver(func: Callable[[webdriver.Chrome], T]) -> T:
+    """Handles WebDriver setup, execution, and teardown."""
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless=new")  # Run in headless mode
+    driver = webdriver.Chrome(options=options)
+    try:
+        return func(driver)
+    finally:
+        # print("Tear down")
+        driver.quit()
+
+def get_date_event_links(home_link: str) -> List[str]:
+    """Fetches date event links from the homepage."""
+    def fetch_links(driver: webdriver.Chrome) -> List[str]:
+        driver.get(home_link)
+        home_page = HomePage(driver)
+        return home_page.get_date_event_links()
+    
+    return with_driver(fetch_links)
+
+def get_match_links_for_a_date(date_link: str) -> List[str]:
+    """Fetches match links for a given date."""
+    def fetch_links(driver: webdriver.Chrome) -> List[str]:
+        driver.get(date_link)
+        date_page = DatePage(driver)
+        return date_page.get_match_links()
+    
+    return with_driver(fetch_links)
+
+def get_bet_data_for_a_date(date_link: str) -> List:
+    """Fetches betting data list for matches in a day."""
+    def fetch_bet_data(driver: webdriver.Chrome):
+        date_data = []
+        driver.get(date_link)
+        date_page = DatePage(driver)
+        match_links = date_page.get_match_links()
+        for match_link in match_links:
+            print(match_link)
+            driver.get(match_link)
+            match_page = MatchPage(driver)
+            bet_data = match_page.get_bet_data()
+            print(bet_data)
+            date_data.append(bet_data)
+        return date_data
+    
+    return with_driver(fetch_bet_data)
+
+def get_bet_data_for_a_match(match_link: str):
+    """Fetches betting data for a match."""
+    def fetch_bet_data(driver: webdriver.Chrome):
+        driver.get(match_link)
+        match_page = MatchPage(driver)
+        return match_page.get_bet_data()
+    
+    return with_driver(fetch_bet_data)
+
